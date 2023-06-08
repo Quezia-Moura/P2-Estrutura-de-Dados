01371c00d0f243340267ea28bb767542be951c7e
JoaoLopes07 <133873524+JoaoLopes07@users.noreply.github.com>
2023-06-07 23:50:27 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -3,22 +3,27 @@ canino = {'Tipo: ': 'Cão', 'Idade: ': '2', 'Cor: ': 'Bege', 'Porte: ': 'Médio'
 def cadastrar_animal():
     ...
     print('\n\tINFORME DETALHES SOBRE O ANIMAL')
+
     t = input('Tipo: ')
     while not t.isalpha():
         print('Tipo inválido!')
         t = input('Tipo: ')
+
     i = input('Idade: ')
     while not i.isnumeric():
         print('Idade inválida!')
         i = input('Idade: ')
+
     c = input('Cor: ')
     while not c.isalpha():
         print('Cor inválida!')
         c = input('Cor: ')
+
     p = input('Porte: ')
     while not p.isalpha():
         print('Porte inválido!')
         p = input('Porte: ')
+
     part = input('Particularidade: ')
     while not part.isalpha():
         print('Particularidade inválida!')
@@ -29,10 +34,12 @@ def cadastrar_animal():
 def info_pessoa():
     ...
     print('\n\tINFORME OS DADOS PESSOAIS')
+
     nome = input('Nome: ')
     while not nome.isalpha():
         print('Nome inválido!')
         nome = input('Nome: ')
+
     cpf = input('CPF sem traços e pontos: ')
     while not cpf.isnumeric():
         print('Cpf inválido!')
@@ -43,11 +50,14 @@ def info_pessoa():
     if len(cpf) == 11:
         cpf_formatado = cpf[0:3] + '.' + cpf[3:6] + '.' + cpf[6:9] + '-' + cpf[9:]
     print(cpf_formatado)
+
     idade = input('Idade: ')
     while not idade.isnumeric():
         print('Idade inválida!')
         idade = input('Idade: ')
+
     endereço = input(str('Endereço: '))
+    
     telefone = input('Telefone com DDD: ')
     while not telefone.isnumeric():
         print('Telefone inválido!')
