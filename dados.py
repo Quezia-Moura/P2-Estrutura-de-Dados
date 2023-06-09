8b5ee7ff42168b293329d716a78b4b2430f9a1ed
Quézia Moura <125207561+Quezia-Moura@users.noreply.github.com>
2023-06-08 21:24:50 -0300

    separando os dicionários e listas

---
+++ dados.py
@@ -0,0 +1,16 @@
+canino = {'Tipo: ': 'Cão', 'Idade: ': '2', 'Cor: ': 'Bege', 'Porte: ': 'Médio', 'Particularidade: ': 'Não'}
+
+
+tipo = list(range(1,51))
+idade = list(range(1,51))
+cor = list(range(1,51))
+porte = list(range(1,51))
+particularidade = list(range(1,51))
+
+
+especies = {'canino':[tipo, idade, cor, porte, particularidade], 
+            'felinos':[tipo, idade, cor, porte, particularidade],
+            'aves':[tipo, idade, cor, porte, particularidade],
+            'peixes':[tipo, idade, cor, porte, particularidade],
+            'outros':[tipo, idade, cor, porte, particularidade]}
+
