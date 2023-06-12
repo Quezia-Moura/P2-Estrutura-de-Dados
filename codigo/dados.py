beb988b5a31958f1bdcb9531353bcba415bf9ae0
Quézia Moura <125207561+Quezia-Moura@users.noreply.github.com>
2023-06-11 23:44:49 -0300

    Delete dados.py

--- codigo/dados.py
+++
@@ -1,9 +0,0 @@
-can = {'Tipo': [], 'Idade': [], 'Cor': [], 'Porte': [], 'Particularidade': []}
-fel = {'Tipo': [], 'Idade': [], 'Cor': [], 'Porte': [], 'Particularidade': []}
-ave = {'Tipo': [], 'Idade': [], 'Cor': [], 'Porte': [], 'Particularidade': []}
-pei = {'Tipo': [], 'Idade': [], 'Cor': [], 'Porte': [], 'Particularidade': []}
-out = {'Tipo': [], 'Idade': [], 'Cor': [], 'Porte': [], 'Particularidade': []}
-
-
-
-pessoas = {'Nome': [], 'CPF': [], 'Idade': [], 'Endereço': [], 'telefone': []}
