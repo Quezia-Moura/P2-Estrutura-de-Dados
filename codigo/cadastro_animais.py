08ab4d31b356bbe2109e88bbe82390ffd5fc0065
Quézia Moura <125207561+Quezia-Moura@users.noreply.github.com>
2023-06-12 18:50:54 -0300

    Update and rename cadastro.py to cadastro_animais.py

--- codigo/cadastro_animais.py
+++ codigo/cadastro_animais.py
@@ -7,8 +7,6 @@ dados_aves = open('dados_aves.txt', 'a', encoding='utf-8')
 dados_peixes = open('dados_peixes.txt', 'a', encoding='utf-8')
 dados_outros = open('dados_outros.txt', 'a', encoding='utf-8')
 
-dados_pessoas = open('dados_pessoas.txt', 'a', encoding='utf-8')
-
 
 def canino():
     print('\n\tINFORME DETALHES SOBRE O ANIMAL')
@@ -38,8 +36,8 @@ def canino():
         print('Particularidade inválida!')
         part = input('Particularidade: ')
 
-    dados_canino.write(f'Tipo: {t}, Idade: {i}, Cor: {c}, Porte: {p}, Particularidade: {part}')
-
+    dados_canino.write(f'{t} {i} {c} {p} {part}\n')
+    dados_canino.close()
 
 def felinos():
     print('\n\tINFORME DETALHES SOBRE O ANIMAL')
@@ -69,8 +67,8 @@ def felinos():
         print('Particularidade inválida!')
         part = input('Particularidade: ')
 
-    dados_felinos.write(f'Tipo: {t}, Idade: {i}, Cor: {c}, Porte: {p}, Particularidade: {part}')
-
+    dados_felinos.write(f'{t} {i} {c} {p} {part}\n')
+    dados_felinos.close()
 
 def aves():
     print('\n\tINFORME DETALHES SOBRE O ANIMAL')
@@ -100,8 +98,8 @@ def aves():
         print('Particularidade inválida!')
         part = input('Particularidade: ')
 
-    dados_aves.write(f'Tipo: {t}, Idade: {i}, Cor: {c}, Porte: {p}, Particularidade: {part}')
-
+    dados_aves.write(f'{t} {i} {c} {p} {part}\n')
+    dados_aves.close()
 
 def peixes():
     print('\n\tINFORME DETALHES SOBRE O ANIMAL')
@@ -131,8 +129,8 @@ def peixes():
         print('Particularidade inválida!')
         part = input('Particularidade: ')
 
-    dados_peixes.write(f'Tipo: {t}, Idade: {i}, Cor: {c}, Porte: {p}, Particularidade: {part}')
-
+    dados_peixes.write(f'{t} {i} {c} {p} {part}\n')
+    dados_peixes.close()
 
 def outros():
     print('\n\tINFORME DETALHES SOBRE O ANIMAL')
@@ -162,7 +160,8 @@ def outros():
         print('Particularidade inválida!')
         part = input('Particularidade: ')
 
-    dados_outros.write(f'Tipo: {t}, Idade: {i}, Cor: {c}, Porte: {p}, Particularidade: {part}')
+    dados_outros.write(f'{t} {i} {c} {p} {part}\n')
+    dados_outros.close()
 
 
 def cadastrar_animal():
@@ -198,84 +197,6 @@ def cadastrar_animal():
     if op_e == '5':
         limpar()
         outros()    
-
-
-def info_pessoa():
-    limpar()
-    print('\n\tINFORME OS DADOS PESSOAIS')
-
-    n = input('Nome: ')
-    while not n.isalpha():
-        print('Nome inválido!')
-        n = input('Nome: ')
-
-    cpf = input('CPF: ')
-    while not cpf.isnumeric():
-        print('CPF inválido!\nDigite apenas números!')
-        cpf = input('CPF: ')
-    while len(cpf) < 11:
-        print('CPF inválido!\nO CPF informado deve conter 11 digitos!')
-        cpf = input('CPF: ')
-
-    i_p = input('Idade: ')
-    while not i_p.isnumeric():
-        print('Idade inválida!')
-        i_p = input('Idade: ')
-
-    end = input(str('Endereço: '))
-
-    tel = input('Telefone com DDD: ')
-    while not tel.isnumeric():
-        print('Telefone inválido!')
-        tel = input('Telefone com DDD: ')
-    while len(tel) < 11:
-        print('telefone inválido!')
-        tel = input('Telefone com DDD: ')
-   
-    dados_pessoas.write(f'\nNome: {n}, CPF: {cpf}, Idade: {i_p}, Endereço: {end}, Telefone: {tel}')
-
-
-def quer_doar():
-    limpar()
-    cadastrar_animal()
-
-
-def quer_adotar():
-    limpar()
-    especie = str(input('Qual espécie quer adotar? '))
-    preferencia = str(input('Possui alguma preferência? '))
-
-    with open('dados_pessoas.txt', 'a'):
-        dados_pessoas.write(f' | Espécie que quer adotar: {especie}, Preferência: {preferencia}')
-
-
-def doar_e_adotar():
-    limpar()
-    cadastrar_animal()
-    quer_adotar()
-
-
-def cadastrar_pessoa():
-    limpar()
-    info_pessoa()
-    limpar()
-    print('''
-    ||===============================||
-    ||                               ||
-    ||    ( 1 ) QUER DOAR            ||
-    ||    ( 2 ) QUER ADOTAR          ||
-    ||    ( 3 ) DOAR E ADOTAR        || 
-    ||                               ||
-    ||===============================||
-    ''')
-    o = input('Selecione uma opção: ')
-    if o == '1':
-        quer_doar()
-    if o == '2':
-        quer_adotar()
-    if o == '3':
-        doar_e_adotar()
-
 
 
 def limpar():
